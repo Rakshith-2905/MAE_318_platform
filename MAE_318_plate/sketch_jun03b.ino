f7f452e3031f475135e2f7f8c3ecb9f1ce9dd001
Rakshith S <rakshith.2905@gmail.com>
2017-06-03 18:34:47 -0700

    Add files via upload

---
+++ MAE_318_plate/sketch_jun03b.ino
@@ -0,0 +1,113 @@
+/*
+The sensor outputs provided by the library are the raw 16-bit values
+obtained by concatenating the 8-bit high and low magnetometer data registers.
+They can be converted to units of gauss using the
+conversion factors specified in the datasheet for your particular
+device and full scale setting (gain).
+
+Example: An LIS3MDL gives a magnetometer X axis reading of 1292 with its
+default full scale setting of +/- 4 gauss. The GN specification
+in the LIS3MDL datasheet (page 8) states a conversion factor of 6842
+LSB/gauss (where LSB means least significant bit) at this FS setting, so the raw
+reading of 1292 corresponds to 1292 / 6842 = 0.1888 gauss.
+*/
+
+#include <Wire.h>
+#include <LIS3MDL.h>
+
+LIS3MDL mag;
+
+char report[80];
+bool IN_A,IN_B;
+int PinA = 10;
+int PinB = 11;
+float MagneticOffset = 0;
+float head = 0;
+float prev_head = 0;
+float prev_mx = 0;
+float prev_my = 0;
+float prev_mz = 0;
+
+float mx = 0;
+float my = 0;
+float mz = 0;
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+
+  if (!mag.init())
+  {
+    Serial.println("Failed to detect and initialize magnetometer!");
+    while (1);
+  }
+
+  mag.enableDefault();
+  for (int i=0;i<=50;i++){
+    MagneticOffset += HeadingData();
+    delay(10);
+  }
+  MagneticOffset = MagneticOffset/50;
+  
+}
+
+void loop()
+{
+    float angle = HeadingData()-MagneticOffset;
+    Serial.print("  angle  ");
+    Serial.println(angle);
+     // motor (255 ,'ccw');
+
+}
+
+int HeadingData(){
+    mag.read();
+    float a = 0.2;
+    float b = 1;
+    mx = mag.m.x;
+    my = mag.m.y;
+    mz = mag.m.z;
+
+    mx = mx*a+(a-1)*prev_mx;
+    my = my*a+(a-1)*prev_my;
+    mz = mz*a+(a-1)*prev_mz;
+
+    prev_mx = mx;
+    prev_my = my;
+    prev_mz = mz;
+    
+    head=180*atan2(mx,my)/PI;
+    Serial.print("Hx ");
+    Serial.print(mx);
+    Serial.print("  Hy ");    
+    Serial.print(my);
+    
+    Serial.print("  Raw Head  ");
+    Serial.print(head);
+    if(mx < 0 && my <0) head = 360+head;
+    else if(mx < 0 && my >0) head = 360+head; 
+    else if(my == 0 && mx <0) head = 180;
+    else if(my == 0 && mx >0) head = 0;
+
+    head = head*b+(b-1)*prev_head;
+    prev_head = head;
+    Serial.print("  Head  ");
+    Serial.print(head);
+//    delay(200);
+    return head;
+}
+//void motor(char direc){
+//  if(direc == 'ccw'){
+//    IN_A = HIGH;
+//    IN_B = LOW;
+//  }
+//  if(direc == 'cw'){
+//    IN_A = LOW;
+//    IN_B = HIGH;
+//  }
+//  digitalWrite(PinA , IN_A);
+//  digitalWrite(PinB , IN_B);  
+//}
+
---
+++ MAE_328_bot/MAE_328_bot.ino
@@ -0,0 +1,158 @@
+#include <Encoder.h>
+
+
+///////////////////////////////////////////////////////////
+//Motor Constants
+///////////////////////////////////////////////////////////
+
+//User has chosen the constants associated with the standard rear differential
+//drive version of the robot with the Pololu 50:1 micro metal geared motors.
+
+float encoderCountsPerRotation = 12; // Encoder counts per shaft rotation.
+float MotorGearRatio = 51.45; // The gearing ratio of the drive motor being used.
+float WheelDiameter = 3.2; // Wheel Diameter in cm.
+float AxelLength = 12.9; // Axel length in cm.
+
+int R1 = 9;
+int R2 = 10;
+int L1 = 11;
+int L2 = 12;
+
+///////////////////////////////////////////////////////////
+//Motor PID Control Constants
+///////////////////////////////////////////////////////////
+
+//Motor Controller Timers
+float PIDMotorsTimeStartLeft;
+float PIDMotorsTimeStartRight;
+
+//Integral Error Holders
+float integralRight;
+float integralLeft;
+float diffRight;
+float diffLeft;
+float errorRight;
+
+//Motor PID Control Constants
+float kpRightMotor = 1;
+float kiRightMotor = 0;
+float kdRightMotor = 0;
+ 
+float kpLeftMotor = 1;
+float kiLeftMotor = 0;
+float kdLeftMotor = 0;
+
+
+///////////////////////////////////////////////////////////
+//Encoder Things
+///////////////////////////////////////////////////////////
+
+/* Change these two numbers to the pins connected to your encoder.
+   Best Performance: both pins have interrupt capability
+   Good Performance: only the first pin has interrupt capability
+   Low Performance:  neither pin has interrupt capability */
+Encoder RightEnc(2, 7);
+Encoder LeftEnc(3, 6);
+
+int motorLeftVolt;//Right HBridge, Left Motor Motor Input Voltage (Arduino PWM Units, int 0-255)
+int motorRightVolt;//Right HBridge, Right Motor Motor Input Voltage (Arduino PWM Units, int 0-255)
+
+float oldSpeedErrorLeft; //Left HBridge, Left Motor old rotation speed error for PIDMotorControl Function
+float oldSpeedErrorRight; //Left HBridge, Right Motor old rotation speed error for PIDMotorControl Function
+
+int oldMotorPIDEncoderCountLeft;//Left Motor old Encoder Count storage for PIDMotorControl Function
+int oldMotorPIDEncoderCountRight;//Right Motor old Encoder Count storage for PIDMotorControl Function
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(R1,OUTPUT);
+  pinMode(R2,OUTPUT);
+  pinMode(L1,OUTPUT);
+  pinMode(L2,OUTPUT);
+}
+
+
+void loop() {
+  PIDMotorControlRight(10);
+  PIDMotorControlLeft(10);
+
+}
+
+void PIDMotorControlRight(float desVel){
+    //Keeps the rotational speeds of the individual motors at setpoints desVel (rad/s).
+
+    float timeStep = 10;
+
+    if (millis() - PIDMotorsTimeStartRight >= timeStep){
+      float PIDTimeStep = (millis() - PIDMotorsTimeStartRight)/1000.0;//Time step for controller to work on (s).
+      
+      int countRight = RightEnc.read();
+
+      // Error on individual motors for vel control
+      float errorRight = desVel - 2.0 * PI * (countRight - oldMotorPIDEncoderCountRight) / (encoderCountsPerRotation * MotorGearRatio * PIDTimeStep);
+      integralRight = errorRight * PIDTimeStep;
+      float diffRight = (errorRight - oldSpeedErrorRight) / PIDTimeStep;
+      oldSpeedErrorRight = errorRight;
+      oldMotorPIDEncoderCountRight = countRight;
+
+      motorRightVolt += int(kpRightMotor*errorRight + kiRightMotor*integralRight + kdRightMotor*diffRight);
+
+      if (motorRightVolt>255){
+        motorRightVolt=255;
+      }
+      if (motorRightVolt<-255){
+        motorRightVolt=-255;
+      }
+      if (motorRightVolt >= 0){
+        analogWrite(R1, motorRightVolt);
+        analogWrite(R2, 0);
+      }
+      if (motorRightVolt < 0){
+        analogWrite(R2, motorRightVolt);
+        analogWrite(R1, 0);
+      }
+      
+      Serial.println(errorRight);
+      PIDMotorsTimeStartRight = millis();
+    }
+}
+
+void PIDMotorControlLeft(float desVel){
+    //Keeps the rotational speeds of the individual motors at setpoints desVel (rad/s).
+
+    float timeStep = 10;
+
+    if (millis() - PIDMotorsTimeStartLeft >= timeStep){
+      float PIDTimeStep = (millis() - PIDMotorsTimeStartLeft)/1000.0;//Time step for controller to work on (s).
+      
+      int countLeft = LeftEnc.read();
+
+      // Error on individual motors for vel control
+      float errorLeft = desVel - 2.0 * PI * (countLeft - oldMotorPIDEncoderCountLeft) / (encoderCountsPerRotation * MotorGearRatio * PIDTimeStep);
+      integralLeft = errorLeft * PIDTimeStep;
+      float diffLeft = (errorLeft - oldSpeedErrorLeft) / PIDTimeStep;
+      oldSpeedErrorLeft = errorLeft;
+      oldMotorPIDEncoderCountLeft = countLeft;
+
+      motorLeftVolt += int(kpLeftMotor*errorLeft + kiLeftMotor*integralLeft + kdLeftMotor*diffLeft);
+
+      if (motorLeftVolt>255){
+        motorLeftVolt=255;
+      }
+      if (motorLeftVolt<-255){
+        motorLeftVolt=-255;
+      }
+      if (motorLeftVolt >= 0){
+        analogWrite(R1, motorLeftVolt);
+        analogWrite(R2, 0);
+      }
+      if (motorLeftVolt < 0){
+        analogWrite(R2, motorLeftVolt);
+        analogWrite(R1, 0);
+      }
+      
+      Serial.println(errorLeft);
+      PIDMotorsTimeStartLeft = millis();
+    }
+}
